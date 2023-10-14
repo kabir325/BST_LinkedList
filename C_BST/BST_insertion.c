8507ee108e6e74d1bde3c8908713b32efdc4b39a
kabir325 <119738554+kabir325@users.noreply.github.com>
2023-10-14 23:38:48 +0530

    Update BST_insertion.c

--- C_BST/BST_insertion.c
+++ C_BST/BST_insertion.c
@@ -14,3 +14,10 @@ struct node* insert(struct node* root, int data) {
 
     return root;
 }
+
+/*
+    to insert a given value in a BST we recursively iterate the function till the perfect position of the value is found according to the rule of BST
+    root=NULL
+    we assign memory to the node using DMA and then assign its pointers to NULL and assign the value given and insert it in the position given.
+    we also have a condition when the value we are trying to insert is already present in the bst in that case we wont insert the value we will just return the orignal BST.
+*/
