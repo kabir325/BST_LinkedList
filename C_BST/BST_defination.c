0381bc04df230630dbcf271f9f858448f01c66f2
kabir325 <119738554+kabir325@users.noreply.github.com>
2023-10-14 23:34:58 +0530

    Update BST_defination.c

--- C_BST/BST_defination.c
+++ C_BST/BST_defination.c
@@ -6,3 +6,8 @@ struct node{
     struct node *left;
     struct node *right;
 };
+
+/*
+    this is the code for the structure of BST in c language.
+    every node has one data variable and two adjescsnt child node pointers.
+*/
