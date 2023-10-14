d18a002c5d529c74f896d2e6a94cab9621dbeec0
kabir325 <119738554+kabir325@users.noreply.github.com>
2023-10-15 02:18:28 +0530

    Create BST_postorder_traversal.c

---
+++ C_BST/BST_postorder_traversal.c
@@ -0,0 +1,11 @@
+void postorderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+/*
+
+*/
